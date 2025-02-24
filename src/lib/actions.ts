ee930b841fb73252ea19bbab7cdcdb03ca4c7b91
April <95392008+apriltaoyvr@users.noreply.github.com>
2025-02-24 01:27:38 -0800

    Implement API
    Remove export of 'bot' to satisfy Next.js routes.ts
    Update action.ts to work in production
    Update README.md

---
+++ src/lib/actions.ts
@@ -0,0 +1,8 @@
+'use server';
+
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://ipa-ref.vercel.app' : 'http://localhost:3000';
+
+export async function getWord(word: string) {
+  const response = fetch(`${baseUrl}/api/${word}`).then((res) => res.json());
+  return response;
+}
