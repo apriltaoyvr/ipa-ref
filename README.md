bab337f1c653d47b740f6c7ee701458e5924e41a
April <95392008+apriltaoyvr@users.noreply.github.com>
2025-03-03 00:00:39 -0800

    Update IPAKey and README to show sources

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # IPA Reference
 
-A site for looking up IPA [(International Phonetic Alphabet)](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) transcriptions for words in English.
+A site for looking up IPA [(International Phonetic Alphabet)](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) transcriptions for words in English using the [Wikimedia API for Wiktionary](http://en.wiktionary.org/w/api.php) and [Merriam-Webtster's Learner Dictionary API](https://dictionaryapi.com/products/api-learners-dictionary).
+
 I created this site for any other linguistics fans, researchers, or language learners who want to find IPA transcriptions quickly (i.e. with less clicks on sites that provide them).
 
 ## Screenshots
-![{2FF14A19-1B09-4153-9887-DB6AF5824C55}](https://github.com/user-attachments/assets/0bb4753a-b7bb-40a2-9583-5d3410296980)
 
+![{2FF14A19-1B09-4153-9887-DB6AF5824C55}](https://github.com/user-attachments/assets/0bb4753a-b7bb-40a2-9583-5d3410296980)
 
 ## TODO
 
 - Add a UK dictionary
-- TTS (if there are options that don't cost $$$)
--- src/components/ipa-key/IPAKey.tsx
+++ src/components/ipa-key/IPAKey.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
   Sheet,
@@ -15,29 +16,30 @@ import { OtherTable } from './tables/other-table';
 export function IPAKey() {
   return (
     <Sheet>
-      {/* Identical classes to Button default */}
       <SheetTrigger asChild>
-        <Button variant='outline' className='hover:cursor-pointer'>IPA Key</Button>
+        <Button variant='outline' className='hover:cursor-pointer'>
+          IPA Key
+        </Button>
       </SheetTrigger>
       <SheetContent side='left'>
         <SheetHeader>
           <SheetTitle>IPA Key</SheetTitle>
           <SheetDescription>
-            A quick cheat sheet for IPA characters
+            A quick cheat sheet for IPA characters from{' '}
+            <Link
+              className='hover:text-muted-foreground/90 underline font-semibold'
+              href='https://en.wikipedia.org/wiki/Help:IPA/English#Key'
+            >
+              Wikipedia
+            </Link>
           </SheetDescription>
         </SheetHeader>
-        <section className='p-2 overflow-y-auto'>
+        <section className='overflow-y-auto p-2'>
           <Tabs defaultValue='consonants'>
-            <TabsList className='mb-4 place-self-center sticky top-0 z-50'>
-              <TabsTrigger value='consonants'>
-                Consonants
-              </TabsTrigger>
-              <TabsTrigger value='vowels'>
-                Vowels
-              </TabsTrigger>
-              <TabsTrigger value='other'>
-                Other
-              </TabsTrigger>
+            <TabsList className='sticky top-0 z-50 mb-4 place-self-center'>
+              <TabsTrigger value='consonants'>Consonants</TabsTrigger>
+              <TabsTrigger value='vowels'>Vowels</TabsTrigger>
+              <TabsTrigger value='other'>Other</TabsTrigger>
             </TabsList>
             <TabsContent value='consonants'>
               <ConsonantTable />
